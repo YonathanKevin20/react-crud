801e5b7b481bef71a7b7aa8756d927c83adbd7e7
Kevin <yonathan.kevin20@gmail.com>
2019-07-12 09:45:29 +0700

    Updates

---
+++ precache-manifest.601a56e9e65b1c627481046c393c1d05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6723a0c8c025e29533dd3c4fd3f373",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "428fbbc974d319b02160",
+    "url": "/react-crud/static/css/main.e96f5903.chunk.css"
+  },
+  {
+    "revision": "7ae087d2b30ab49e183b",
+    "url": "/react-crud/static/js/2.75876e6e.chunk.js"
+  },
+  {
+    "revision": "428fbbc974d319b02160",
+    "url": "/react-crud/static/js/main.cefcf3f9.chunk.js"
+  },
+  {
+    "revision": "2f3d0c7cc7993cab9d36",
+    "url": "/react-crud/static/js/runtime~main.73d447a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-crud/static/media/logo.5d5d9eef.svg"
+  }
+]);
